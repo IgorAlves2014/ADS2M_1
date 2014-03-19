4d9dbfca755660cdceb3af6580e9d92b807c1752
IgorAlves2014 <igor_rosa_alves@hotmail.com>
2014-03-19 11:16:16 -0300

    hello world
    trab

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		out.println("Hello World!");
+
+	
+	}
+
+}
